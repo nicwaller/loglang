2e761fb2583c2ac99820a41bea728630227e15df
Nic Waller <code@nicwaller.com>
2023-10-29 14:25:15 -0700

    [gzip] fix gzip
    there was a race condition with the underlying slice of []byte getting mutated before being fully written to stdout that corrupted the gzip data

--- framing/gzip.go
+++ framing/gzip.go
@@ -3,6 +3,7 @@ package framing
 import (
 	"compress/gzip"
 	"context"
+	"fmt"
 	"github.com/nicwaller/loglang"
 	"io"
 )
@@ -30,6 +31,7 @@ func (p *gzipFraming) Extract(ctx context.Context, input <-chan []byte, output c
 	// io.Pipe MUST be paired with a goroutine to avoid blocking
 	go loglang.PumpToWriter(ctx, stop, input, pipeWriter)
 	loglang.PumpFromReader(ctx, stop, subreader, output)
+	close(output)
 
 	return
 }
@@ -39,15 +41,34 @@ func (p *gzipFraming) Frameup(ctx context.Context, input <-chan []byte, output c
 	ctx, stop = context.WithCancelCause(ctx)
 	ctx = context.WithValue(ctx, loglang.ContextKeyPluginType, "framing[gzip]")
 
-	readCompressed, pipeWriter := io.Pipe()
-	writePlain, retErr := gzip.NewWriterLevel(pipeWriter, gzip.BestSpeed)
-	if retErr != nil {
-		return
+	w := gzip.NewWriter(channelWriter{ch: output})
+	loglang.PumpToWriter(ctx, stop, input, w)
+
+	// close the gzip writer to flush the remaining bytes
+	err := w.Close()
+	if err != nil {
+		return fmt.Errorf("failed to flush gzip: %w", err)
 	}
 
-	// io.Pipe MUST be paired with a goroutine to avoid blocking
-	go loglang.PumpToWriter(ctx, stop, input, writePlain)
-	loglang.PumpFromReader(ctx, stop, readCompressed, output)
+	// then close the output channel to indicate that framing is complete
+	close(output) // all framing plugins must close output to signal completion!
 
 	return
+}
+
+type channelWriter struct {
+	ch chan<- []byte
+}
+
+func (cw channelWriter) Write(unsafeBytes []byte) (n int, err error) {
+	// !! We MUST make a copy of the slice given to us by gzip !!
+	// because the gzip implementation reuses the underlying buffer to start preparing the next chunk
+	// so if we don't make a copy now, the data is likely to mutate before it gets written to outputs
+	// and that results in corrupted gzip output.
+	//
+	// yes there is a performance impact, but correctness is more important.
+	safeCopy := make([]byte, len(unsafeBytes))
+	copy(safeCopy, unsafeBytes)
+	cw.ch <- safeCopy
+	return len(safeCopy), nil
 }
